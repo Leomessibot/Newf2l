50e8ac42d31db48f0b380038253897c27db5d325
Leomessibot <151534752+Leomessibot@users.noreply.github.com>
2025-03-13 17:22:25 +0400

    Update database.py

--- biisal/utils/database.py
+++ biisal/utils/database.py
@@ -1,6 +1,3 @@
-# (c) TechifyBots
-# (c) @biisal
-#(c) Adarsh-Goel
 import datetime
 import motor.motor_asyncio
 
@@ -11,6 +8,7 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.bannedList = self.db.bannedList
+        self.channels = self.db.channels  # Collection for storing approved channels
 
     def new_user(self, id):
         return dict(
@@ -21,15 +19,15 @@ class Database:
     async def add_user(self, id):
         user = self.new_user(id)
         await self.col.insert_one(user)
-        
+
     async def add_user_pass(self, id, ag_pass):
         await self.add_user(int(id))
         await self.col.update_one({'id': int(id)}, {'$set': {'ag_p': ag_pass}})
-    
+
     async def get_user_pass(self, id):
         user_pass = await self.col.find_one({'id': int(id)})
         return user_pass.get("ag_p", None) if user_pass else None
-    
+
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id': int(id)})
         return True if user else False
@@ -44,34 +42,42 @@ class Database:
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
-        
-    async def ban_user(self , user_id):
-        user = await self.bannedList.find_one({'banId' : int(user_id)})
+
+    async def ban_user(self, user_id):
+        user = await self.bannedList.find_one({'banId': int(user_id)})
         if user:
             return False
         else:
-            await self.bannedList.insert_one({'banId' : int(user_id)})
+            await self.bannedList.insert_one({'banId': int(user_id)})
             return True
-        
-    async def is_banned(self , user_id):
-        user = await self.bannedList.find_one({'banId' : int(user_id)})
+
+    async def is_banned(self, user_id):
+        user = await self.bannedList.find_one({'banId': int(user_id)})
         return True if user else False
-    
-    async def is_unbanned(self , user_id):
-        try : 
-            if await self.bannedList.find_one({'banId' : int(user_id)}):
-                await self.bannedList.delete_one({'banId' : int(user_id)})
+
+    async def is_unbanned(self, user_id):
+        try:
+            if await self.bannedList.find_one({'banId': int(user_id)}):
+                await self.bannedList.delete_one({'banId': int(user_id)})
                 return True
             else:
                 return False
         except Exception as e:
-            e = f'Fᴀɪʟᴇᴅ ᴛᴏ ᴜɴʙᴀɴ.Rᴇᴀsᴏɴ : {e}'
-            print(e)
+            print(f"Failed to unban. Reason: {e}")
             return e
 
-      async def add_channel(self , channel_id):
-        return await self.db.add("approved_channels", {"channel_id": channel_id})
+    # Add a new channel for link generation
+    async def add_channel(self, channel_id):
+        channel = await self.channels.find_one({"channel_id": int(channel_id)})
+        if channel:
+            return False  # Channel already added
+        await self.channels.insert_one({"channel_id": int(channel_id)})
+        return True  # Channel successfully added
 
-      async def get_channel(self , channel_id):
-          return await self.db.find_one("approved_channels", {"channel_id": channel_id})
-     
+    # Check if a channel is already approved
+    async def get_channel(self, channel_id):
+        return await self.channels.find_one({"channel_id": int(channel_id)})
+
+    # Remove a channel (if needed)
+    async def remove_channel(self, channel_id):
+        await self.channels.delete_one({"channel_id": int(channel_id)})
