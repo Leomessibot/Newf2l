09bdd0cf4f8e5ad18ac9d010e5963a16c3e4e73f
Leomessibot <151534752+Leomessibot@users.noreply.github.com>
2025-03-13 17:11:12 +0400

    Update database.py

--- biisal/utils/database.py
+++ biisal/utils/database.py
@@ -69,8 +69,8 @@ class Database:
             print(e)
             return e
 
-     async def add_channel(self, channel_id):
+     async def add_channel(self , channel_id):
         return await self.db.add("approved_channels", {"channel_id": channel_id})
 
-    async def get_channel(self, channel_id):
+    async def get_channel(self , channel_id):
         return await self.db.find_one("approved_channels", {"channel_id": channel_id})
